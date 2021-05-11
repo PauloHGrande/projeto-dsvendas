3dd660ae487f5c1ca08f66974806ea910d32e04c
phg <paulo.grande@matera.com>
2021-05-11 11:22:05 -0300

    Criando Componentes e add no App.tsx

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/PauloHGrande" target="_blank" rel="noreferrer">Paulo Henrique</a></p>
+                <p className="text-light"><small><strong>Spring React</strong></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
