41b23193682af45548d385f2b915d50b4fda0b2a
phg <paulo.grande@matera.com>
2021-05-17 16:35:25 -0300

    Paginação no DataTable

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,30 @@
+import { type } from 'os';
+import React from 'react';
+import { SalePage } from 'types/sale';
+
+type Props = {
+    page: SalePage;
+    onPageChange: Function;
+}
+
+const Pagination = ( { page, onPageChange } : Props ) => {
+    return (
+        <div className="col-sm-12 d-flex justify-content-center">
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.first ? 'disabled' : ''} `}>
+                        <button className="page-link btn btn-primary btn-lg" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link btn btn-primary btn-lg">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''} `}>
+                        <button className="page-link btn btn-primary btn-lg" onClick={() => onPageChange(page.number + 1)}>Proxima</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Pagination;
