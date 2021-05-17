2d50658f5669f2445c777e7571cedb0bad8ac094
phg <paulo.grande@matera.com>
2021-05-17 09:23:31 -0300

    Navegação com Rotas

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Pagina Home.</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Exibir um dashboard a partir de dados fornecidos por um back end construido com Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acesso DashBoard.
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
